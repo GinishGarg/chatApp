bbb5afcc0ed9c34e4ef1e419cf44ba4995a2c15c
Ginish Garg <kg209675@gmail.com>
2024-07-16 01:33:03 +0530

    App is deployed

--- api/server.js
+++ api/server.js
@@ -5,17 +5,29 @@ import userRoutes from "./routes/user.route.js"
 import dotenv from "dotenv"
 import connectToMongo from "./db/connectToDb.js";
 import cookieParser from "cookie-parser";
-import { app  ,server } from "./socket/socket.js"; 
+import { app  ,server } from "./socket/socket.js";
+import path from "path"
 
 dotenv.config();
 
 // const app = express();
 
+const __dirname = path.resolve()
+
+
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 
 server.listen(5000,()=>{
     connectToMongo();
